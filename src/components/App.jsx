cb2f1716b0ce6d870118ecd99c736c31313049db
unknown <lakshmisriramadari1427@gmail.com>
2023-05-25 20:22:52 +0530

    modified chat interface

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import ChatInterface from './ChatInterface';
 function App(){
   return (
     <div className="app">
-      <h1>Chatbot Example</h1>
+      <h1 className='head'>ChatGPT Bot</h1>
       <ChatInterface />
     </div>
   );
--- src/components/ChatInterface.jsx
+++ src/components/ChatInterface.jsx
@@ -1,14 +1,46 @@
-import React from 'react';
+
+import React, { useState } from 'react';
+import SendIcon from '@mui/icons-material/Send';
+import { Configuration, OpenAIApi } from "openai";
+require('dotenv').config();
+// const OPENAI_API_KEY=;
+const configuration = new Configuration({
+    organization: "org-uGXeezsTf4DQgEOLJuG3oHAc",
+    apiKey:process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 const ChatInterface = () => {
+
+    const [prompt,setPrompt]=useState("");
+    const [messages,setMessages]=useState([]);
+    function handleChange(event){
+        setPrompt(event.target.value);
+    }
+    function handleClick(event){
+      
+        setMessages([...messages,prompt]);
+        setPrompt("");
+        openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [{role: "user", content: "Hello world"}],
+        }).then((response)=>{
+          console.log(response.data);
+        }).catch((error)=>{
+          console.log("error");
+        })
+    }
+
+
   return (
     <div className="chat-interface">
       <div className="chat-window">
         {/* Display chat messages */}
       </div>
       <div className="input-container">
-        <input type="text" placeholder="Type your message..." />
-        <button>Send</button>
+        <input onChange={handleChange} type="text" placeholder="Type your message..." value={prompt}/>
+        <button onClick={handleClick}><SendIcon/></button>
       </div>
     </div>
   );
